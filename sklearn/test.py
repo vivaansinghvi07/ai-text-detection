28dab774eb5d5e93b3290c5e5bf4491ae7acef34
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-11 18:58:00 -0500

    started accuracy test for sklearn

---
+++ sklearn/test.py
@@ -0,0 +1,14 @@
+# gets create file that i made ( for the process method )
+import create
+
+# import packages
+import csv
+import pickle
+
+# gets sklearn and needed things
+import sklearn
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# creates test arrays
+wikiDataTest = 
