892a818e11a2e278c0cb6d93d193fa3ef17d56ba
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-11 19:52:01 -0500

    finished create model function

--- v2-sklearn/create.py
+++ v2-sklearn/create.py
@@ -11,23 +11,26 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score
 
+# gets reshaping from numpy
+from numpy import reshape
+
 
 # get stopwords from nltk
 """
 nltk.download(['stopwords'])
 """
 
-template.print("Getting data...")
+template.clprint("Getting data...")
 
 # array for train data and test data
 trainDataX, trainDataY, testDataX, testDataY = [], [], [], []
 
 # how many lines are read and put into the model
-DATACOUNT = 50000
+DATACOUNT = 500
 
 # gets the data from the wiki
 wikiDataTrainX, wikiDataTrainY = template.getWikiData(0, DATACOUNT)
-wikiDataTestX, wikiDataTestY = template.getWikiData(DATACOUNT, DATACOUNT + 10000)
+wikiDataTestX, wikiDataTestY = template.getWikiData(DATACOUNT, DATACOUNT + 100)
 
 # puts data into train and test
 trainDataX += wikiDataTrainX
@@ -43,19 +46,19 @@ testDataVecX = vectorizer.transform(testDataX)
 # creates model
 classifier = MultinomialNB()
 
-template.print("Training model...")
+template.clprint("Training model...")
 
 # trains model
 classifier.fit(trainDataVecX, trainDataY)
 
-template.print("Saving model...")
+template.clprint("Saving model...")
 
 # saves model
 with open('models/sklearnmodel.pkl', "wb") as f:
     pickle.dump(classifier, f)
 
-template.print("Testing model...")
+template.clprint("Testing model...")
 
 # runs accuracy test
-testPredictedY = classifier.predict(testDataX)
-print("The accuracy of the model is " + str(accuracy_score(testDataY, testPredictedY)) + ".")
+testPredictedY = classifier.predict(testDataVecX)
+template.clprint("The accuracy of the model is " + str(accuracy_score(testDataY, testPredictedY)) + ".")
